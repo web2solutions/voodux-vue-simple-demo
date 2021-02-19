61d0c2b77e29c562b729d48902654c1b0d0fb92c
Eduardo Almeida <web2solucoes@gmail.com>
2021-02-19 19:58:35 -0300

    Initial commit

---
+++ src/components/DepositsChart.vue
@@ -0,0 +1,14 @@
+<template>
+  <canvas class="my-4 w-100" id="myChart" width="900" height="250"></canvas>
+</template>
+
+
+<script>
+import DepositsChart from './DepositsChart.vm.js'
+export default DepositsChart
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+// import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
+
+// Vue.use(BootstrapVue)
+
+/* new Vue({
+  el: '#app',
+  render: h => h(App)
+}) */
 
 createApp(App).mount('#app')
